afba3eb63b47022850afe00306971526482880a6
Petteri Taro <ext-petteri.taro@casambi.com>
2021-12-13 04:14:22 +0200

    1.2 tehty

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,5 +1,39 @@
 import React from 'react'
 
+const Header = (props) => {
+  return(
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      <Part name={props.part1} amount={props.exercises1}/>
+      <Part name={props.part2} amount={props.exercises2}/>
+      <Part name={props.part3} amount={props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.name} {props.amount}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,19 +43,23 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content 
+      part1={part1} 
+      part2={part2} 
+      part3={part3} 
+      exercises1={exercises1}
+      exercises2={exercises2}
+      exercises3={exercises3}
+      />
+      <Total 
+      exercises1={exercises1}
+      exercises2={exercises2}
+      exercises3={exercises3}
+      />
     </div>
   )
 }
