6c4063f9069280977d1093664f5267fda38b6d06
Petteri Taro <ext-petteri.taro@casambi.com>
2021-12-14 04:05:10 +0200

    1.11 tehty

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -9,13 +9,31 @@ const Statistics = ({ good, neutral, bad}) => {
   return(
     <div>
       <h1>statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {good+bad+neutral}</div>
-      <div>average {(good-bad)/(good+bad+neutral)}</div>
-      <div>positive {good/(good+bad+neutral)}%</div>
+      <table>
+      <StatisticLine text="good" value={good}/>
+      <StatisticLine text="neutral" value={neutral}/>
+      <StatisticLine text="bad" value={bad}/>
+      <StatisticLine text="all" value={good+bad+neutral}/>
+      <StatisticLine text="average" value={(good-bad)/(good+bad+neutral)}/>
+      <StatisticLine text="positive" value={(good/(good+bad+neutral))*100} unit='%'/>
+      </table>
     </div>
+  )
+}
+
+const Button = ({ clickEvent, label }) => {
+  return (
+    <button onClick={clickEvent}>{label}</button>
+  )
+}
+
+const StatisticLine = ({ text, value, unit }) => {
+  return(
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+      <td>{unit}</td>
+    </tr>
   )
 }
 
@@ -28,9 +46,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button clickEvent={() => setGood(good+1)} label='good'/>
+      <Button clickEvent={() => setNeutral(neutral+1)} label='neutral'/>
+      <Button clickEvent={() => setBad(bad+1)} label='bad'/>
 
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
